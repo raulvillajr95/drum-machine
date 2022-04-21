8d594e5872ad923fad85178085cad478e12906a1
Raul <raulvillajr95@gmail.com>
2022-04-21 12:26:31 -0700

    Add index.html, style.css, and scripts.js files

---
+++ scripts.js
@@ -0,0 +1,14 @@
+let keyQ = document.getElementById('Q')
+function soundQ() {
+  keyQ.play()
+}
+
+let keyW = document.getElementById('W')
+function soundW() {
+  keyW.play()
+}
+
+let keyE = document.getElementById('E')
+function soundE() {
+  keyE.play()
+}
