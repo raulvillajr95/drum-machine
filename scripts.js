c82d2697c7e6fc3ca699f477f31976f19051cdc5
Raul <raulvillajr95@gmail.com>
2022-04-21 15:57:16 -0700

    Fully functional 'Drum Machine' project from fcc

--- scripts.js
+++ scripts.js
@@ -1,52 +1,61 @@
+let disp = document.getElementById('display')
+
 let keyQ = document.getElementById('Q')
 function soundQ() {
   keyQ.play()
+  disp.innerHTML = 'Camera'
 }
 
 let keyW = document.getElementById('W')
 function soundW() {
   keyW.play()
+  disp.innerHTML = 'Thank you';
 }
 
 let keyE = document.getElementById('E')
 function soundE() {
   keyE.play()
+  disp.innerHTML = 'Sonic stop';
 }
 
 let keyA = document.getElementById('A')
 function soundA() {
   keyA.play()
+  disp.innerHTML = 'Bubble';
 }
 
 let keyS = document.getElementById('S')
 function soundS() {
   keyS.play()
+  disp.innerHTML = 'Nintendo Switch';
 }
 
 let keyD = document.getElementById('D')
 function soundD() {
   keyD.play()
+  disp.innerHTML = 'Horse';
 }
 
 let keyZ = document.getElementById('Z')
 function soundZ() {
   keyZ.play()
+  disp.innerHTML = 'Punch';
 }
 
 let keyX = document.getElementById('X')
 function soundX() {
   keyX.play()
+  disp.innerHTML = 'Kick';
 }
 
 let keyC = document.getElementById('C')
 function soundC() {
   keyC.play()
+  disp.innerHTML = 'Pop';
 }
 
 window.addEventListener('keydown', function(event) {
   const key = event.key;
-  
-  console.log(key)
   switch(key) {
     case 'q':
     case 'Q':
